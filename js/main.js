aaa3c4cb8003a4701c126e5e3c5fe86cef462da7
jpmione <jpmione@gmail.com>
2015-03-09 14:21:51 -0400

    added js, css, photos and more style

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    $("#show").click(function(){
+        $("#photos").show();
+    });
+});
+
+$(function(){
+	$(".thumbnail").hover(function(){
+   		$(this).stop().animate({"opacity": "1"}); 
+	}, function(){
+   		$(this).stop().animate({"opacity": "0.5"});
+	});
+
+	$("img.thumbnail").on("click", function() {
+		var imgTag = "<img class='focused' src='" + this.src + "'>";
+		$("#lightbox").show();
+		$("body").append(imgTag);
+		$("body").addClass("noscroll");
+	});
+	$("#lightbox").on("click", function() {
+		$(this).hide();
+		$(".focused").remove();
+		$("body").removeClass("noscroll");
+	});
+});
